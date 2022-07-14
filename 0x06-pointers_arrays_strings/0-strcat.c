18146670f3ec5d9705906fd423db51f9b2867558
Ivordean <ivordean1@gmail.com>
2022-07-14 09:58:07 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strcat - this a funtion strcat
- * @dest: The destination string
- * @src: The source string
- * Return: A pointer to the resulting string dest
- */
-char *_strcat(char *dest, char *src)
+* _memset - Entry point
+* @s: pointed destination
+* @b: constant byte
+* @n: bytes
+* Return: Always 0 (Success)
+*/
+char *_memset(char *s, char b, unsigned int n)
 {
-int dlen = 0, i;
-while (dest[dlen])
-{
-dlen++;
-}
-for (i = 0; src[i] != 0; i++)
-{
-dest[dlen] = src[i];
-dlen++;
-}
-dest[dlen] = '\0';
-return (dest);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return (s);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
-#ifndef MAIN H
-#define MAIN H
+#ifndef MAIN_H
+#define MAIN_H
 int  _putchar(char c);
-
+char *_strcat(char *dest, char *src);
+#endif/** MAIN_H */
