e8956a2df8e2dd96a8c329a04d11ebe5ca6ab2e0
Ivordean <ivordean1@gmail.com>
2022-07-15 08:59:20 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -5,18 +5,19 @@
  * @a: array a
  * @n: an element of an array
  */
-void reverse_array(int *a, int n)
+void reverse_array(int a, int n)
 {
-	int *p, i, aux, k;
+	int p, i, aux, k;
 
-	p =a;
+	p = a;
 	for (i = 0; i < n; i++)
 		p++;
 	for (k = 0; k < n / 2; k++)
 	{
 		aux = a[k];
-		a[k] = *p;
-		*p = aux;
+		a[k] = p;
+		p = aux;
 		p--;
 	}
+
 }
