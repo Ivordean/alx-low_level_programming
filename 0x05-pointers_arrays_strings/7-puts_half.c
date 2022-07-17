32204335e0d9260f39b0cdc49f8c59b40b06aa3e
Ivor-Dean Hammond <107547602+Ivordean@users.noreply.github.com>
2022-07-17 13:44:07 +0200

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,39 +1,21 @@
 #include "main.h"
+#include <string.h>
+
 /**
-*puts_half - puts half of a string
-*@str: string
-*Return:void
+* puts_half ->  puts half
+* @str: string parameter
 */
 void puts_half(char *str)
 {
-int len = _strlen(str);
+	int x, y, i;
 
-if (len % 2 == 0)
-len = len / 2;
+	x = strlen(str);
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2;
+	for (i = y; i < x; i++)
+	_putchar(str[i]);
+	_putchar('\n');
 
-else
-len = (len + 1) / 2;
-
-while (str[len] != '\0')
-{
-_putchar(str[len]);
-len++;
-}
-_putchar('\n');
-}
-
-
-/**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
- */
-int _strlen(char *s)
-{
-int count, inc;
-inc = 0;
-for (count = 0; s[count] != '\0'; count++)
-inc++;
-
-return (inc);
 }
