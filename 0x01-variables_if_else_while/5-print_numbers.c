0db2507de6d4f4ae190d6df3c3d3cc5678478cea
Ivor-Dean Hammond <107547602+Ivordean@users.noreply.github.com>
2022-07-16 17:49:28 -0700

    5-print_numbers.c
    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: "prints all single digit numbers of base 10 starting from 0"
+* Return: always return 0 (success)
+*/
+
+int main(void)
+
+{
+	printf("0123456789\n");
+	return (0);
+}
