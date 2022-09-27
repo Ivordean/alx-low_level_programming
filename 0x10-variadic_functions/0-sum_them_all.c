c6a5280295b89f4c0731912ad82ccef95f36f822
Ivordean <ivordean1@gmail.com>
2022-09-27 10:55:00 -0700

    variadic funtions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,4 @@
-#include "variadic_functions.h"
-#include <stdarg.h>
+include <stdarg.h>
 
 /**
 *sum_them_all - sums all its parameters
@@ -27,4 +26,6 @@ sum += va_arg(li, int);
 va_end(li);
 return (sum);
 
+}
+~
 }
